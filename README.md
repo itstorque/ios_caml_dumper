cadc5a83746e776c2be5e50bd636597bc298f590
Tareq El Dandachi <tareqdandachi@gmail.com>
2020-07-05 20:58:04 +0400

    Added scp script generation and a dump of CAML files form iOS 13.5

--- README.md
+++ README.md
@@ -1,11 +1,23 @@
-# List CAML files in directory
-This is a script that extracts certain file types from a directory.
+# CAML filedump from iOS 13.5
 
-[output.txt](output.txt) contains the list of CAML files in /System/Library on an iOS 13.5 device, this should help locate CAML resources for glyphs.
+**NOTE**: None of the files in the directory [ios-13.5-glyphs](ios-13.5-glyphs)
+are created/owned by the author of this repo, they are simply a copy of some
+files found in iOS's `/System/Library/` directory.
+
+[extract_files.py](extract_files.py) is a script that extracts certain file types
+from a directory, specifically `.caml` files by default. It then generates two
+files:
+
+* [output.txt](output.txt) contains the list of CAML files in /System/Library on
+an iOS 13.5 device, this should help locate CAML resources for glyphs.
+
+* [scp_command.sh](scp_command.sh) contains an scp command that can be run to dump
+the files from your iOS device. More information on running scp_command.sh can be
+found in the [Running The Output Script](#Running-The-Output-Script) section.
 
 ## Usage
 
-`extract_files.py -i <inputfile> -o <outputfile> -e <extension>`
+`extract_files.py -i <inputfile> -o <outputfile> -s <script_file> -e <extension> -p <port> -u <user_and_host>`
 
 The default values are:
 
@@ -14,3 +26,20 @@ The default values are:
 `outputfile = 'output.txt'`
 
 `extension  = 'caml'`
+
+`script_file = 'scp_command.sh'`
+
+`port = 2022`
+
+`user_and_host  = 'root@localhost'`
+
+## Running The Output Script
+
+The output script file is meant to be run in an empty target directory, if the directory isn't empty, the script won't run.
+
+Give the file execution permission and run it in the target directory to populate it with the output CAML files.
+
+```bash
+chmod +x [$path_to_script_file]
+[$path_to_script_file]
+```
--- extract_files.py
+++ extract_files.py
@@ -1,24 +1,31 @@
 #!/usr/local/bin/python3
-
 """
     This program is meant to be run on the output of
     `ls -laR` on the directory /System/Library on iOS.
 """
 
 import sys, getopt
+from random import sample
 
 def main(argv):
    inputfile  = ''
    outputfile = ''
    extension  = ''
+   port = ''
+   user_and_host = ''
+   script_file = ''
+
    try:
-      opts, args = getopt.getopt(argv,"hi:o:e:",["input=","output=","extension="])
+      opts, args = getopt.getopt(argv,"hi:o:e:p:u:",["input=","output=","extension=","port=","user_and_host="])
+
    except getopt.GetoptError:
-      print ('extract_files.py -i <inputfile> -o <outputfile> -e <extension>')
+      print ('extract_files.py -i <inputfile> -o <outputfile> -e <extension> -s <script_file> -p <port> -u <user_and_host>')
       sys.exit(2)
+
    for opt, arg in opts:
+
         if opt == '-h':
-            print ('extract_files.py -i <inputfile> -o <outputfile> -e <extension>')
+            print ('extract_files.py -i <inputfile> -o <outputfile> -e <extension> -s <script_file> -p <port> -u <user_and_host>')
             sys.exit()
         elif opt in ("-i", "--input"):
             inputfile = arg
@@ -26,12 +33,18 @@ def main(argv):
             outputfile = arg
         elif opt in ("-e", "--ext", "--extension"):
             extension = arg.split(".")[-1]
+        elif opt in ("-p", "--port"):
+            port = arg
+        elif opt in ("-u", "--user_and_host", "-h", "--host"):
+            user_and_host = arg
+        elif opt in ("-s", "--script_file", "--script"):
+            script_file = arg
 
-   return inputfile, outputfile, extension
+   return inputfile, outputfile, extension, port, user_and_host
 
 if __name__ == "__main__":
 
-    inputfile, outputfile, extension = main(sys.argv[1:])
+    inputfile, outputfile, extension, port, user_and_host = main(sys.argv[1:])
 
     extension = '.caml' if extension=="" else "."+extension
 
@@ -42,6 +55,7 @@ if __name__ == "__main__":
 
     contains_caml = False
     directory = None
+    directories = set()
     files = set()
 
     output = ""
@@ -59,8 +73,8 @@ if __name__ == "__main__":
 
         if len(line) == 1:
             if contains_caml:
-                print(directory[:-1], "->", files)
-                output = output + directory[:-1] + " -> " + str(files) + "\n"
+                directories.add(directory[2:-2])
+                output = output + directory[:-2] + "/ -> " + str(files) + "\n"
             directory = None
             files = set()
             contains_caml = False
@@ -74,4 +88,51 @@ if __name__ == "__main__":
 
     file = open("output.txt" if outputfile == '' else outputfile, "w")
     file.write("List of .caml files\n\n" + output)
+    file.close()
+
+    file = open("scp_command.sh" if script_file == '' else script_file, "w")
+
+	# add a check to see if directory is empty
+    scp_command = """if [ "$(ls -A ./)" ]; then \n
+		echo "This directory is not empty..."
+	else
+		"""
+
+	# use this block if the length of the command is too long to send
+	# over, however it should be fine in most cases...
+
+    # while len(directories) > 0:
+	#
+    #     dir_subset = set(sample(directories, min(40, len(directories))))
+	#
+    #     directories -= dir_subset
+	#
+    #     scp_command += "scp -r -P " + \
+	# 		str(port if port else 2022) + " " + \
+	# 		str(user_and_host if user_and_host else "root@localhost") + \
+	# 		":/System/Library/\{"
+	#
+    #     # triple escape in command line because it is being sent to the remote and
+	# 	# needs to be escaped there, double that to escape in python string i.e.
+	# 	# 6 backslashes in total :)
+    #     scp_command += '/*.caml,'.join(dir_subset).replace(" ", "\\\\\\ ")
+	#
+    #     scp_command += "/*.caml\} ./\n"
+
+    scp_command += "scp -r -P " + \
+        str(port if port else 2022) + " " + \
+        str(user_and_host if user_and_host else "root@localhost") + \
+        ":/System/Library/\{"
+
+    scp_command += ','.join(directories).replace(" ", "\\\\\\ ")
+
+    scp_command += "\} ./\n"
+
+    scp_command += "find ./ -maxdepth 10 -type f ! -name \"*.caml\" -delete \n"
+
+    scp_command += "find ./ -type d -empty -delete \n"
+
+    scp_command += "\nfi"
+
+    file.write("#!/bin/bash\n\n"+scp_command)
     file.close()
